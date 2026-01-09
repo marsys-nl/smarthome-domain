5a0ec3abb7e829c84e1c3c9062bbb5f9c84ed20e
Niels Marsman <niels.marsman@gmail.com>
2026-01-09 17:11:44 +0100

    SMART11 - Initial project commit containing basic gradle project setup

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+rootProject.name = "smarthome-domain"
+
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+include(":library")
