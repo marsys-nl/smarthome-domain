70fad7b3eab4b11a8a091f677473e8f601250712
Niels Marsman <niels.marsman@gmail.com>
2026-06-02 10:38:46 +0200

    SMART-16 - Implemented detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,15 @@
 plugins {
+    alias(libs.plugins.detekt) apply true
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.test.balloon) apply false
+}
+
+detekt {
+    allRules = true
+    buildUponDefaultConfig = true
+    config.from("$rootDir/config/detekt/detekt.yml")
+
+    source.from(
+        "$rootDir/library/src/commonMain/kotlin",
+    )
 }
